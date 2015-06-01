eeebfbbf961a01a57fd602c4e8bce36d1a0c58a1
xuxihao <958338150@qq.com>
2015-06-01 23:11:54 +0800

    Register实现

--- src/com/nankai/yimixicanandroid/activity/RegisterActivity.java
+++ src/com/nankai/yimixicanandroid/activity/RegisterActivity.java
@@ -1,17 +1,22 @@
 package com.nankai.yimixicanandroid.activity;
 
 
-import com.nankai.yimixicanandroid.R;
-
+import android.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
-
-public class Register extends Activity {
+import android.widget.Button;
+import android.widget.EditText;
+//步骤1：继承父类Android.app.Activity
+public class RegisterActivity extends Activity {
+	// 步骤4：声明该页面中的交互类组件
+	private EditText txtName, txtPassword,txtRPassword;
+	private Button btnRegister, btnCancel;
 
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
+	// 步骤2：重写父类中的一个方法OnCreate()  Ctrl+Shift+S,V
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
