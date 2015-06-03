9ec06d05598aa99f47ce90c758379d5711b248b6
xuxihao <958338150@qq.com>
2015-06-03 12:34:20 +0800

    注册代码bug修补

--- src/com/nankai/yimixicanandroid/activity/RegisterActivity.java
+++ src/com/nankai/yimixicanandroid/activity/RegisterActivity.java
@@ -1,17 +1,30 @@
 package com.nankai.yimixicanandroid.activity;
 
+import java.util.ArrayList;
+import java.util.List;
 
-import android.R;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.nankai.yimixicanandroid.R;
+import com.nankai.yimixicanandroid.internet.WebAccessUtils;
+import com.nankai.yimixicanandroid.po.Users;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 //步骤1：继承父类Android.app.Activity
 public class RegisterActivity extends Activity {
 	// 步骤4：声明该页面中的交互类组件
-	private EditText txtName, txtPassword,txtRPassword;
-	private Button btnRegister, btnCancel;
+	private EditText txtregAccount, txtregPassword,txtregPassword2;
+	private Button btnReg;
 
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
@@ -21,10 +34,47 @@ public class RegisterActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE); //设置全屏
 		this.setContentView(R.layout.activity_register);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE); //设置全屏
+		
+		this.txtregAccount = (EditText) this.findViewById(R.id.txtregAccount);
+		this.txtregPassword = (EditText) this.findViewById(R.id.txtregPassword);
+		this.txtregPassword2 = (EditText) this.findViewById(R.id.txtregPassword2);
+		
+		this.btnReg = (Button) this.findViewById(R.id.btnReg);
+		
 	}
 	
-	
-
+	private class ViewOcl implements View.OnClickListener{
+		public void onClick(View v) {
+			if(R.id.btnReg==v.getId())
+			{
+				String account=txtregAccount.getText().toString().trim();
+				String password=txtregPassword.getText().toString().trim();
+				String repassword=txtregPassword2.getText().toString().trim();
+				Users users=new Users();
+				users.setUsername(account);	
+				users.setPassword(password);			
+				users.setRepassword(repassword);
+				Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-ddhh:mm:ss").create();
+				String user_data = gson.toJson(users);
+				System.out.println(user_data);
+				
+				List<NameValuePair> lstNameValuePairs = new ArrayList<NameValuePair>();
+				lstNameValuePairs.add(new BasicNameValuePair("user_data", user_data));
+		
+				String response = WebAccessUtils.httpRequest("ARegisterServlet", lstNameValuePairs);	
+				System.out.println(response);
+				if(response!="101"&&response!="102")
+				{
+					Toast.makeText(getApplicationContext(), "注册成功！", Toast.LENGTH_LONG).show();
+					Intent intent1 = new Intent();
+					intent1.setClass(RegisterActivity.this, Main.class);
+					startActivity(intent1);
+				}
+				
+				
+			}
+		}
+	}
 }
--- src/com/nankai/yimixicanandroid/po/Users.java
+++ src/com/nankai/yimixicanandroid/po/Users.java
@@ -1,4 +1,4 @@
-package com.nankai.yimixicanandroid.po;
+package com.nankai.yimixican.po;
 
 import java.io.Serializable;
 
@@ -14,19 +14,21 @@ public class Users implements Serializable {
 	private int gender;
 	private int age;
 	private int rank;
-	private String remark;
+	private String remind;
 	private String password;
 	private float height;
 	private float weight;
-	private String head_photo;
-	private String segtime;
+	private String remindtime;
+	private String repassword;
+	private String declaration;
 	public Users() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 	public Users(int uid, String username, String regtime, int gender, int age,
-			int rank, String remark, String password, float height,
-			float weight, String head_photo, String segtime) {
+			int rank, String remind, String password, float height,
+			float weight, String remindtime, String repassword,
+			String declaration) {
 		super();
 		this.uid = uid;
 		this.username = username;
@@ -34,13 +36,15 @@ public class Users implements Serializable {
 		this.gender = gender;
 		this.age = age;
 		this.rank = rank;
-		this.remark = remark;
+		this.remind = remind;
 		this.password = password;
 		this.height = height;
 		this.weight = weight;
-		this.head_photo = head_photo;
-		this.segtime = segtime;
+		this.remindtime = remindtime;
+		this.repassword = repassword;
+		this.declaration = declaration;
 	}
+	
 	public int getUid() {
 		return uid;
 	}
@@ -77,11 +81,11 @@ public class Users implements Serializable {
 	public void setRank(int rank) {
 		this.rank = rank;
 	}
-	public String getRemark() {
-		return remark;
+	public String getRemind() {
+		return remind;
 	}
-	public void setRemark(String remark) {
-		this.remark = remark;
+	public void setRemind(String remind) {
+		this.remind = remind;
 	}
 	public String getPassword() {
 		return password;
@@ -101,17 +105,23 @@ public class Users implements Serializable {
 	public void setWeight(float weight) {
 		this.weight = weight;
 	}
-	public String getHead_photo() {
-		return head_photo;
+	public String getRemindtime() {
+		return remindtime;
 	}
-	public void setHead_photo(String head_photo) {
-		this.head_photo = head_photo;
+	public void setRemindtime(String remindtime) {
+		this.remindtime = remindtime;
 	}
-	public String getSegtime() {
-		return segtime;
+	public String getRepassword() {
+		return repassword;
 	}
-	public void setSegtime(String segtime) {
-		this.segtime = segtime;
+	public void setRepassword(String repassword) {
+		this.repassword = repassword;
+	}
+	public String getDeclaration() {
+		return declaration;
+	}
+	public void setDeclaration(String declaration) {
+		this.declaration = declaration;
 	}
 	public static long getSerialversionuid() {
 		return serialVersionUID;
@@ -120,11 +130,11 @@ public class Users implements Serializable {
 	public String toString() {
 		return "Users [uid=" + uid + ", username=" + username + ", regtime="
 				+ regtime + ", gender=" + gender + ", age=" + age + ", rank="
-				+ rank + ", remark=" + remark + ", password=" + password
-				+ ", height=" + height + ", weight=" + weight + ", head_photo="
-				+ head_photo + ", segtime=" + segtime + "]";
+				+ rank + ", remind=" + remind + ", password=" + password
+				+ ", height=" + height + ", weight=" + weight + ", remindtime="
+				+ remindtime + ", repassword=" + repassword + ", declaration="
+				+ declaration + "]";
 	}
 	
-	
-	
+
 }
